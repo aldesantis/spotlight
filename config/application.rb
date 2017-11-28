f64c308493d043fd3b5b31381d9eb82933a55ede
Alessandro Desantis <desa.alessandro@gmail.com>
2017-11-28 23:22:14 +0100

    Remove ActionCable

--- config/application.rb
+++ config/application.rb
@@ -5,7 +5,6 @@ require 'active_record/railtie'
 require 'action_controller/railtie'
 require 'action_mailer/railtie'
 require 'active_job/railtie'
-require 'action_cable/engine'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
