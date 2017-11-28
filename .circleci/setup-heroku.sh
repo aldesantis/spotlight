26bb5672a431f0eaba16c39b1b496e8e188ae1e2
Alessandro Desantis <desa.alessandro@gmail.com>
2017-11-28 23:06:43 +0100

    Update CircleCI configuration

--- .circleci/setup-heroku.sh
+++ .circleci/setup-heroku.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 git remote add heroku-staging git@heroku.com:b911-spotlight-staging.git
-git remote add heroku-production git@heroku.com:b911-spotlight-production.git
+git remote add heroku-production git@heroku.com:b911-spotlight-prod.git
 
 cat >> ~/.ssh/config << EOF
   VerifyHostKeyDNS yes
