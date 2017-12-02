04fd95d32d8dfd3420562c6122405500fa06dc94
Alessandro Desantis <desa.alessandro@gmail.com>
2017-12-02 21:08:13 +0100

    Handle errors when performing analysis

--- app/interactors/analyses/perform.rb
+++ app/interactors/analyses/perform.rb
@@ -5,6 +5,30 @@ module Analyses
     accept :analysis
     required :analysis
 
+    delegate :project, :commit, to: :analysis
+
     organize MarkRunning, Checkout, ParseConfig, MarkCompleted
+
+    around :handle_errors
+
+    private
+
+    # rubocop:disable Lint/RescueWithoutErrorClass
+    def handle_errors(interactor)
+      interactor.call
+    rescue => e
+      analysis.update! status: :failed
+
+      project.octokit.create_status(
+        project.repo_uri,
+        commit,
+        'error',
+        context: ENV.fetch('GITHUB_CONTEXT'),
+        description: 'Spotlight could not analyze your commit.'
+      )
+
+      raise e
+    end
+    # rubocop:enable Lint/RescueWithoutErrorClass
   end
 end
